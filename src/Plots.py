da2176794be3559fb67c235d5322754db9065f63
Maria <carrizomascarell@terrasysgeo.com>
2024-04-17 09:34:43 +0200

    added functions

--- src/Plots.py
+++ src/Plots.py
@@ -24,3 +24,51 @@ def PlotModelData(model, depths, data, ax=None, model_name='', model_style='k', 
 
     plt.tight_layout()
     
+def PlotModel(model, depths, ax=None, model_name=None, model_style='k'):
+    if ax is None:
+        fig, ax = plt.subplots()
+    fs=7
+    ax.step(model, depths, model_style, label=model_name)
+    ax.set_xscale('log')
+    ax.set_xlabel('Electrical conductivity [mS/m]')
+    ax.set_ylabel('Depth [m]')
+    if model_name is not None:
+        ax.legend(fontsize=fs)
+    plt.tight_layout()
+    
+def PlotErrorSpace_2Lay(model, model_est, err, models_err, depthmax=10):
+      
+    fig, ax = plt.subplots()
+    
+    x = (models_err[:,0])*1000 # conductivities of first layer in mS/m
+    y = models_err[:,2]          # thickness of first layer
+    z = err
+    
+    ngridx = 100
+    ngridy = 200
+    
+    # Create grid values first.
+    xi = np.linspace(np.min(x), np.max(x), ngridx)
+    yi = np.linspace(np.min(y), np.max(y), ngridy)
+    
+    # Linearly interpolate the data (x, y) on a grid defined by (xi, yi).
+    triang = tri.Triangulation(x, y)
+    interpolator = tri.LinearTriInterpolator(triang, z)
+    Xi, Yi = np.meshgrid(xi, yi)
+    zi = interpolator(Xi, Yi)
+    
+    ax.contour(xi, yi, zi*100, levels=20, linewidths=0.5, colors='k')
+    cntr1 = ax.contourf(xi, yi, zi*100, levels=20, cmap="RdBu_r")
+    ax.plot(x, y, '.k', ms=1)
+    ax.set(xlim=(7,10), ylim=(3.4,4.3))
+    ax.scatter(((model_est[0])*1000), model_est[2],
+                 marker ='^', c='y', label='Estimated model')
+    ax.set_xlabel('$\sigma_1$ [mS/m]', fontsize=8)
+    ax.set_ylabel('$h_1$ [m]', fontsize=8)
+    ax.legend()
+    ax.tick_params(axis='both',labelsize=9)
+    #ax[0].tick_params(axis='both',labelsize=9)
+    clb = fig.colorbar(cntr1, ax=ax)
+    clb.ax.set_title('RMS Error %')
+    clb.ax.tick_params(labelsize=9)
+    
--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,8 @@ def rel_diff(data_ini, data_end):
     """ calculates percentage relative difference between two vectors"""
     relative_difference = np.abs((data_ini - data_end)/data_ini)*100
     return relative_difference
+
+def rmse(y_obs, y_pred):
+    """ calculate the root mean squared error """
+    e = np.sqrt(np.sum(((y_obs-y_pred)/y_obs)**2))
+    return e
